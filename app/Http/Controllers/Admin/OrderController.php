f063072da716a98f4faa559423c30566b2163497
kyoko <kyokoueno4biz@gmail.com>
2020-09-16 23:42:33 +0900

    update0916

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -14,7 +14,7 @@ class OrderController extends Controller
 {
   public function __construct()
   {
-    $this->middleware('auth')->except('index');
+    $this->middleware('auth')->except(['index', 'show']);
   }
 
 
